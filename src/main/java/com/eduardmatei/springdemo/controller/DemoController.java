3edfc4aafbc32c94289c4e9bbe4348a32386cb6a
Eduard <eduardmatei0@yahoo.com>
2019-01-18 19:09:54 +0200

    first commit

---
+++ src/main/java/com/eduardmatei/springdemo/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.eduardmatei.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
